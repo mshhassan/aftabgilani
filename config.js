50229abc70b6c10d1b4fb98f9e30c6119801f070
mshhassan <122960288+mshhassan@users.noreply.github.com>
2026-06-13 16:09:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // System Modules & Main Section Definitions
 const PortfolioConfig = {
     sections: [
+        { id: "profile", title: "Profile Summary", dataRef: "profileData" },
         { id: "education", title: "Educational Qualifications", dataRef: "educationData" },
         { id: "training", title: "Professional Trainings & Lectures", dataRef: "trainingData" },
         { id: "achievement", title: "Honors & Achievements", dataRef: "achievementData" }
