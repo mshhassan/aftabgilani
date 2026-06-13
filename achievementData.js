fe2037f10f2c1d7c7c2056f9fcdff1cba782ff2e
mshhassan <122960288+mshhassan@users.noreply.github.com>
2026-06-13 16:06:24 +0500

    Create achievementData.js

---
+++ achievementData.js
@@ -0,0 +1,11 @@
+const achievementData = [
+    {
+        topic: "CAA DGs Commendation Certificate",
+        authority: "Civil Aviation Authority",
+        date: "Dec 23, 2025",
+        description: "CAA Director Generals Commendation Certificate for exemplary operational resilience management.",
+        baseFolder: "achievementpic",
+        subFolder: "2025-12-23_DC_Commendation",
+        images: ["image1.jpg"]
+    }
+];
