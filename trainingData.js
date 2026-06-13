84b7f95eed5b30d779251c81fc192d60b2376b5b
mshhassan <122960288+mshhassan@users.noreply.github.com>
2026-06-13 16:05:56 +0500

    Create trainingData.js

---
+++ trainingData.js
@@ -0,0 +1,12 @@
+const trainingData = [
+    {
+        category: "AI and Digital Transformation in Aviation Industry",
+        topic: "Emerging Technologies for Safe, Secure and Efficient Aviation",
+        date: "Dec 23, 2025",
+        location: "NUST, Islamabad",
+        description: "Held an interactive session with Civil Engineering students of NUST Islamabad on Modern Trends of Airport Planning, Engineering and Operations at NICE.",
+        baseFolder: "trainingpic", 
+        subFolder: "2025-12-23_AI_Application", 
+        images: ["image1.jpg", "image2.jpg", "image3.jpg"]
+    }
+];
