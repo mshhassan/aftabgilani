355fc84840e4d4b3c5f2cac74f1a59febda03a1d
mshhassan <122960288+mshhassan@users.noreply.github.com>
2026-06-13 16:05:32 +0500

    Create educationData.js

---
+++ educationData.js
@@ -0,0 +1,10 @@
+const educationData = [
+    {
+        topic: "Master of Business Administration (MBA)",
+        university: "Lahore University of Management Sciences (LUMS)",
+        date: "Jun 15, 2018",
+        baseFolder: "educationpic", // Root folder name
+        subFolder: "2018-06-15_LUMS_MBA", // Subfolder name
+        images: ["image1.jpg", "image2.jpg"] // Images inside it
+    }
+];
